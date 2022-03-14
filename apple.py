67b277e54ce90369acff4e2a8245f92f54415989
Arthur <arthurkarimullin2005@gmail.com>
2022-03-14 16:24:49 +0500

    Пробуем добавить цвет

--- apple.py
+++ apple.py
@@ -1,11 +1,14 @@
+from rich.console import Console
+console = Console()
+
 x = 10
 
 for y in range(3):
 	if y ==2:
-		print(' '*(int(x/2)-1),'#')
+	    	console.print(' '*(int(x/2)-1),'#',style='green')
 
 for y in range(5):
 	if y == 0 or y == 4:
-		print('#'*x+'\b','\r','')
+		console.print('#'*x+'\b','\r','')
 	else:
-		print('#'*x)
+		console.print('#'*x)
